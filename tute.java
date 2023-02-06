7381706339f73362488be773eeef3c1434cd4ccb
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-06 11:36:10 +0530

    Switch case

--- tute.java
+++ tute.java
@@ -137,20 +137,64 @@ import java.util.*;
 // }
 // }
 
+// public class tute {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+
+//     if( a == b){
+//         System.out.println("Input is Equal");
+//     }else if(a > b){
+//         System.out.println("A is greater than b");
+//     }else if(a < b){
+//         System.out.println("B is greater than a");
+//     }else{
+//         System.out.println("Input is invalid");
+//     }
+//     }
+// }
+
+// public class tute {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+
+//     if(a >= b && a > b){
+//         System.out.println("Input is greater than and equal to B");
+//     }else{
+//         System.out.println("Input is smaller than");
+//     }
+// }
+// }
+
+// public class tute {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     if (a == 1){
+//         System.out.println("Hello");
+//     }else if(a == 2){
+//         System.out.println("Namaste");
+//     }else if(a == 3){
+//         System.out.println("Bonjour");
+//     }
+//     }
+// }
+
+// ! switch case ::
+
 public class tute {
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    int b = sc.nextInt();
 
-    if( a == b){
-        System.out.println("Input is Equal");
-    }else if(a > b){
-        System.out.println("A is greater than b");
-    }else if(a < b){
-        System.out.println("B is greater than a");
-    }else{
-        System.out.println("Input is invalid");
+    switch(a){
+        case 1: System.out.println("Hello"); break;
+        case 2: System.out.println("Namaste"); break;
+        case 3: System.out.println("Bonjour"); break;
+        default: System.out.println("loda");
     }
     }
 }
