b599a6d3f1c40d9998847908af31217e3985d9f2
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-06 10:22:39 +0530

    0

--- tute.java
+++ tute.java
@@ -63,14 +63,14 @@ import java.util.*;
 //     }
 // }
 
-public class tute {
-public static void main(String[] args){
-    int a = 10;
-    int b = 20;
-    int sum = a+b;
-    System.out.println(sum);
-    }
-}
+// public class tute {
+// public static void main(String[] args){
+//     int a = 10;
+//     int b = 20;
+//     int sum = a+b;
+//     System.out.println(sum);
+//     }
+// }
 
 public class tute {
 public static void main(String[] args){
