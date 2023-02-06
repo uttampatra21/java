3c69fb72a5db220a5b83fe98f3d17714c00dfdc1
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-06 10:19:03 +0530

    first commit

---
+++ tute.java
@@ -0,0 +1,83 @@
+// ! challenge 1
+// public class tute {
+//     public static void main(String[] args){
+//      System.out.println("*");
+//      System.out.println("**");
+//      System.out.println("***");
+//      System.out.println("****");
+//      System.out.println("*****");
+//     }
+// }
+
+//! VARIBLES IN JAVA :-
+//! jAVA IS A TYPED LANGUAGE WHERE YOU HAVE TO DEFINE ITS TYPE BEFORE INITIALIZATION BUT IN JAVASCRIPT WE DONT FOLLOW THIS ARGUMENT;
+
+// public class tute {
+// public static void main(String[] args){
+//     // String name = "Tonty Stark";
+//     // int a = 23;
+//     // int b = 32;
+//     // int age = 34;
+//     // double price = 23.44;
+//     // System.out.print(a+b);
+
+//     // int a = 10;
+//     // int b = 12;
+//     // int sum = a+b;
+//     // int mul = a*b;
+//     // System.out.println(mul);
+//     // System.out.println(a+b);
+//     // System.out.println(sum);
+//     }
+// }
+
+// public class tute {
+// public static void main(String[] args){
+//     int a = 10;
+//     int b = 10;
+//     int sum = a+b;
+//     System.out.println(sum);
+//     }
+// }
+
+
+import java.util.*;
+
+// public class tute {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner (System.in);
+//     String name = sc.nextLine();
+//     System.out.println(name);
+//     }
+// }
+
+//! print a+b
+
+// public class tute {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int i = sc.nextInt();
+//     int b = sc.nextInt();
+//     int sum = i+b;
+//     System.out.println(sum);
+//     }
+// }
+
+// public class tute {
+// public static void main(String[] args){
+//     int a = 10;
+//     int b = 20;
+//     int sum = a+b;
+//     System.out.println(sum);
+//     }
+// }
+
+public class tute {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a+b;
+    System.out.println(sum);
+    }
+}
