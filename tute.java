321b0b6fef9289656bca16d93ac74d588ec83da9
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-07 11:28:03 +0530

    Dose

--- tute.java
+++ tute.java
@@ -339,12 +339,38 @@
 
 // ! challenge 2
 
+// public class tute {
+// public static void main(String[] args){
+//     int n = 4;
+//     for(int i = 1; i <= n; i++){
+//         for (int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+// ! revert the pyramid
+
+// public class tute {
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i = n; i > 1; i--){
+//         for(int j = 1; j < i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
 public class tute {
 public static void main(String[] args){
-    int n = 4;
-    for(int i = 1; i <= n; i++){
-        for (int j = 1; j <= i; j++){
-            System.out.print("*");
+    int n = 5;
+    for(int i = n; i >= 1; i--){
+        for(int j = 0; j < i; j++) {
+            System.out.print("X");
         }
         System.out.println();
     }
