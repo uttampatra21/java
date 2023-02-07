d4bc9047a0e6f24df3f40a58041e4a61907d4544
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-07 10:51:22 +0530

    succes

--- tute.java
+++ tute.java
@@ -41,7 +41,7 @@
 // }
 
 
-import java.util.*;
+// import java.util.*;
 
 // public class tute {
 // public static void main(String[] args){
@@ -305,17 +305,46 @@ import java.util.*;
 //     }
 // }
 
+// public class tute {
+// public static void main(String[] args){
+//     int a = 4;
+//     int b = 5;
+//     for (int i = 1; i <= a; i++){
+//         for(int j =1; j <= b; j++){
+//             if (i == 1 || j == 1 || j == a || j == b){
+//                 System.out.println("*");
+//             }else{
+//                 System.out.println(" ");
+//             }
+//         }
+//     }
+//     }
+// }
+
+// public class tute {
+// public static void main(String[] args){
+//     int a = 4;
+//     int b = 5;
+//     for(int i = 1; i <= a; i++){
+//         for(int j = 1; j <= b; j++){
+//             if(i == 1 || j == 1 || i == a || j == a){
+//                 System.out.print("*");
+//             }else{
+//                 System.out.print(" ");
+//             }
+//         }
+//     }
+//     }
+// }
+
+// ! challenge 2
+
 public class tute {
 public static void main(String[] args){
-    int a = 5;
-    int b = 10;
-    for (int i = 1; i < 5; i++){
-        for(int j =0; j < 5; j++){
-            if (i == a || i == b || j == a || j == b || i == j || j ==i){
-                System.out.println("*");
-            }else{
-                System.out.println("*");
-            }
+    int n = 4;
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            System.out.print("*");
         }
         System.out.println();
     }
