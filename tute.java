996aea67a62b95ffa14f7869d06e671e9d0232d1
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-09 16:05:35 +0530

    0

--- tute.java
+++ tute.java
@@ -436,20 +436,20 @@ import javax.xml.transform.Source;
 //     }
 // }
 
-public class tute {
-public static void main(String[] args){
-    int n = 5;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n-i; j++){
-            System.out.print(" ");
-        }
-        for(int j = 1; j <= i; j++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-    }
-}
+// public class tute {
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= n-i; j++){
+//             System.out.print(" ");
+//         }
+//         for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
 
 // public class tute {
 // public static void main(String[] args){
