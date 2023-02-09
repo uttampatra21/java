d504e3fcb274004484d2d30580cb726dbc5ec75c
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-09 16:08:46 +0530

    0

--- index.java
+++ index.java
@@ -407,19 +407,29 @@ import javax.xml.transform.Source;
 //     }
 // }
 
-public class tute {
+// public class tute {
+// public static void main(String[] args){
+//     int a = 10;
+//     for(int i = 0; i <5; i++){
+//         for(int j = 0; j <i; j++) {
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+public class index {
 public static void main(String[] args){
-    int a = 10;
-    for(int i = 0; i <5; i++){
-        for(int j = 0; j <i; j++) {
-            System.out.print("*");
+    int a = 5;
+    for(int i = 0; i <= a; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print("*" + " ");
         }
         System.out.println();
     }
     }
 }
-
-
 
 // ! PATTERN FOUR
 
