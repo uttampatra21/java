53e26d8bda375131e0cc26092da9de62f6ae02ff
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-09 15:51:17 +0530

    0

--- tute.java
+++ tute.java
@@ -542,3 +542,20 @@
 //     }
 //     }
 // }
+
+public class tute {
+public static void main(String[] args){
+    int a = 5;
+    for(int i = 1; i <= a; i++){
+        for(int j = 1; j <= i; j++){
+            int sum = j+i;
+            if (sum % 2 ==0){
+                System.out.print("1");
+            }else{
+                System.out.print("0");
+            }
+        }
+        System.out.println();
+    }
+    }
+}
