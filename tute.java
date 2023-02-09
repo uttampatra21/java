a13a7426d6dbbd197b10f7385ec2b16af6d13c74
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-09 15:58:17 +0530

    prsctice

--- tute.java
+++ tute.java
@@ -462,6 +462,18 @@
 //     }
 // }
 
+public class tute {
+public static void main(String[] args){
+    int a = 5;
+    for(int i = 1; i <=a; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+    }
+}
+
 // public class tute {
 // public static void main(String[] args){
 //     Scanner sc = new Scanner(System.in);
@@ -489,17 +501,17 @@
 //     }
 // }
 
-public class tute {
-public static void main(String[] args){
-    int a = 5;
-    for (int i = 1; i <= a; i++){
-        for(int j = 1; j <= a-i+1; j++){
-            System.out.print(j+" ");
-        }
-        System.out.println();
-    }
-    }
-}
+// public class tute {
+// public static void main(String[] args){
+//     int a = 5;
+//     for (int i = 1; i <= a; i++){
+//         for(int j = 1; j <= a-i+1; j++){
+//             System.out.print(j+" ");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
 
 // public class tute {
 // public static void main(String[] args){
