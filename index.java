e52aec8dbe3d83a9ae2ec6e528cbf96811a4fd21
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-10 10:23:02 +0530

    number pyramid

--- index.java
+++ index.java
@@ -159,16 +159,46 @@ import javax.xml.transform.Source;
 //     }
 // }
 
+// public class index {
+// public static void main(String[] args){
+//     int n = 4;
+//     for(int i = 1; i <= n; i++){
+
+//         for(int j = 1; j <=(n-i); j++){
+//             System.out.print(" ");
+//         }
+//         for(int j = 1; j <= n; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+// public class index{
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= (n -i); j++){
+//             System.out.print(" ");
+//         }
+//         for(int j = 1; j <=i ; j++){
+//             System.out.print(i+ " ");
+//         }
+//         System.out.println();
+//     }
+//     }
+//     }
+
 public class index {
 public static void main(String[] args){
-    int n = 4;
+    int n = 5;
     for(int i = 1; i <= n; i++){
-
-        for(int j = 1; j <=(n-i); j++){
+        for(int j = 1; j <= (n-i); j++){
             System.out.print(" ");
         }
-        for(int j = 1; j <= n; j++){
-            System.out.print("*");
+        for(int j = 1; j <= i; j++){
+            System.out.print(i +" ");
         }
         System.out.println();
     }
