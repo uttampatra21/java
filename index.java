b79e7e853aacb13cea77759decfb56f30fd6a72c
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-10 08:59:01 +0530

    butterfli/index

--- index.java
+++ index.java
@@ -17,21 +17,91 @@ import javax.xml.transform.Source;
 //     }
 // }
 
- public class index {
- public static void main(String[] args){
-     int n = 4;
-     for(int i = 1; i <= n; i++){
+//  public class index {
+//  public static void main(String[] args){
+//      int n = 4;
+//      for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         int space = 2*(n-i);
+//         for(int j = 1; j <= space; j++){
+//             System.out.print(" ");
+//         }
+//         for (int j = 1; j <= i; j++) {
+//             System.out.print("*");
+//         }
+//         System.out.println();
+        
+//      }
+//      }
+//  }
+
+// public class index {
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         int space = 2 * (n-i);
+//         for(int j = 1; j <= space; j++){
+//             System.out.print(" ");
+//         }
+//         for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//         // LOWER HALF
+//         for(int b = n; b >= 1; b--){
+//             for(int j = 1; j <= ; j++){
+//                 System.out.print("*");
+//             }
+//             // int space = 2*(n-i);
+
+//             for(int j = 1; j <= space; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+//     }
+// }
+
+public class index {
+public static void main(String[] args){
+    int n = 5;
+    // outer loop
+    for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
             System.out.print("*");
         }
-        int space = 2*(n-i);
+        int space = 2 *(n-i);
         for(int j = 1; j <= space; j++){
             System.out.print(" ");
         }
-        for (int j = 1; j <= i; j++) {
+        for(int j = 1; j <= i; j++){
             System.out.print("*");
         }
         System.out.println();
-     }
-     }
- }
+    }
+    // outer loop
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        int space = 2 *(n-i);
+        for(int j = 1; j <= space; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    }
+}
