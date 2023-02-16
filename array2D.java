6c47cfa2639bd2fc2f7b55fc16020c6ea6cf57f8
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-16 13:06:21 +0530

    arry 2d

--- array2D.java
+++ array2D.java
@@ -1,23 +1,72 @@
 import java.util.Scanner;
 
+// import java.util.Scanner;
+
+// public class array2D {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int rows = sc.nextInt();
+//     int cols = sc.nextInt();
+
+//     int numbers [] []  = new int[rows][cols];
+//     // input
+//     for(int i = 0; i<rows; i++){
+//         for(int j = 0; j<cols; j++){
+//             numbers [i] [j] = sc.nextInt();
+//         }
+//     }
+
+//     // output
+//     for(int i = 0; i < rows; i++){
+//         for(int j = 0; j < cols; j++){
+//             System.out.print(numbers [i] [j] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     }
+// }
+
+// public class array2D {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int rows = sc.nextInt();
+//     int cols = sc.nextInt();
+
+//     int numbers [][] = new int [rows][cols];
+//     // input ::
+//     for(int i = 0; i < rows; i++){
+//         for( int j = 0; j< cols; j++){
+//             numbers [i][j] = sc.nextInt();
+//         }
+//     }
+//     // output ::
+//     for(int i = 0; i < rows; i++){
+//         for (int j = 0; j < cols; j++){
+//             System.out.print(numbers[i][j] + " ");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
 public class array2D {
 public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int rows = sc.nextInt();
     int cols = sc.nextInt();
-
-    int numbers [] []  = new int[rows][cols];
+    int [][] numbers = new int [rows][cols];
     // input
-    for(int i = 0; i<rows; i++){
-        for(int j = 0; j<cols; j++){
-            numbers [i] [j] = sc.nextInt();
+    // rows
+    for(int i = 0; i <rows; i++){
+        // cols
+        for(int j = 0; j <cols; j++){
+            numbers [i][j] = sc.nextInt();
         }
     }
-
-    // output
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            System.out.print(numbers [i] [j] + " ");
+    for(int i= 0; i<cols; i++){
+        for(int j = 0; j<cols; j++){
+            System.out.print(numbers[i][j] + " ");
         }
         System.out.println();
     }
