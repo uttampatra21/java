ea7b9fc46c874691e1584ce75792c819402db2bd
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-20 22:15:30 +0530

    0

--- array2D.java
+++ array2D.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 // import java.util.Scanner;
 
@@ -50,26 +50,50 @@ import java.util.Scanner;
 //     }
 // }
 
+// public class array2D {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int rows = sc.nextInt();
+//     int cols = sc.nextInt();
+//     int [][] numbers = new int [rows][cols];
+//     // input
+//     // rows
+//     for(int i = 0; i <rows; i++){
+//         // cols
+//         for(int j = 0; j <cols; j++){
+//             numbers [i][j] = sc.nextInt();
+//         }
+//     }
+//     for(int i= 0; i<cols; i++){
+//         for(int j = 0; j<cols; j++){
+//             System.out.print(numbers[i][j] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     }
+// }
+
 public class array2D {
 public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner (System.in);
     int rows = sc.nextInt();
     int cols = sc.nextInt();
-    int [][] numbers = new int [rows][cols];
-    // input
+    int [][] nembers = new int [rows][cols];
+
+    // input 
     // rows
-    for(int i = 0; i <rows; i++){
-        // cols
-        for(int j = 0; j <cols; j++){
-            numbers [i][j] = sc.nextInt();
+    for(int i = 0; i < rows; i++){
+        for(int j= 0; j< cols; j++){
+            nembers[i][j]= sc.nextInt();
         }
-    }
-    for(int i= 0; i<cols; i++){
-        for(int j = 0; j<cols; j++){
-            System.out.print(numbers[i][j] + " ");
+     }
+    //  output
+    for (int i = 0; i <rows; i++){
+        for(i = 0; i < cols; i++){
+            System.out.println(nembers);
         }
         System.out.println();
     }
-
     }
 }
