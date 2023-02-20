d6eb1d00b967167e5d16d9c0519a4e407b210252
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-20 22:23:40 +0530

    0

--- strings.java
+++ strings.java
@@ -1,6 +1,17 @@
+// public class strings {
+// public static void main(String[] args){
+//     String name = "Uttam patra";
+//     System.out.println(name.length());
+//     }
+// }
+
+
+// we can use loop inn string 
 public class strings {
 public static void main(String[] args){
-    String name = "Uttam patra";
-    System.out.println(name.length());
+    String name = "uttam ptara";
+    for(int i = 0; i<name.length(); i++){
+        System.out.println(name.charAt(i));
+    }
     }
 }
