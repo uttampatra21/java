04f40c328d40e1dd331afed1a8042634074e1f95
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-23 16:18:44 +0530

    0

--- strings.java
+++ strings.java
@@ -6,12 +6,170 @@
 // }
 
 
-// we can use loop inn string 
+// // we can use loop inn string 
+// public class strings {
+// public static void main(String[] args){
+//     String name = "uttam ptara";
+//     for(int i = 0; i<name.length(); i++){
+//         System.out.println(name.charAt(i));
+//     }
+//     }
+// }
+
+// public class strings{
+// public static void main(String[] args){
+//     int sum = 0;
+//     int i = 1;
+//     for( i = 1; i <= 10; i++){
+//        if(i % 2==0){
+//         sum = sum + i;
+//         System.out.println(sum);
+//        }
+//     }
+
+//     }
+// }
+
+
+
+        // int evenSum = 0;
+		// for(int i = 1; i <= num; i++)
+		// {
+		// 	if(i % 2 == 0)
+		// 	{
+		// 		evenSum = evenSum + i; 
+		// 	}
+		// }
+		// System.out.println(evenSum);
+
+
+// public class strings {
+// public static void main(String[] args){
+//     int sum = 0;
+//     for(int i = 0; i<= 10; i++){
+//         if(i % 2 == 1){
+//             sum += i;
+//         }
+//         System.out.println(sum);
+//     }
+//     }
+// }
+
+// public class strings {
+// public static void main(String[] args){
+//     for(int i=0; i <= 10; i++){
+//         for(int j = 0; j < i; j++){
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+// // outerloop 
+//     for(int i=0; i <= 10; i++){
+// // innerloop
+//     for(int j = 0; j < i; j++){
+//         System.out.print("*"); 
+//     }
+//     System.out.println(); //for line breaking
+// }
+
+    // // outerloop 
+    //     for(int i=0; i <= 10; i++){
+    // // innerloop 
+    //     for(int j = 0; j < i; j++){
+            
+    //     System.out.print("*"); // for printing the star
+
+    //     System.out.println(); //for line breaking
+    
+
+// let count = 0;
+//     for (int = left; i < right; i++){
+//         if( i %2 == 0 ){
+//             count = count + 1;
+//         }
+//         sout.println (count);
+//     }
+// for(int i = 1;i<=num;i++){
+//     if(i%3==0 && i%5==0){
+//         System.out.println("FizzBuzzZZ");
+//     }
+//     else if(i%3 == 0){
+//         System.out.println("Fizz");
+//     }
+//     else if(i%5==0){
+//         System.out.println("Buzz");
+//     }
+//     else{
+//         System.out.println(i);
+//     }
+// }
+
+// public class strings {
+// public static void main(String[] args){
+//     int number = 0;
+//     for(int i = 1; i<=7; i++){
+//         if(i%2 != 0){
+//             number += i;
+//         }
+//     }
+//     System.out.println(number);
+//     }
+    
+// }
+
+// public class strings {
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(){
+//                 System.out.print("*");
+//             }
+//         }
+//         System.out.println();
+//     }
+//     }   
+// }
+
+// public class strings {
+// public static void main(String[] args){
+//     int n = 5;
+//     for(int i=n ; i > 0; i--){
+//         System.out.print(i);
+//         for(int j=0; j < n; j++){
+
+//         }
+//     }
+//     }
+// }
+
+// public class strings {
+// public static void main(String[] args){
+//     for(int i=0; i<5; i++){
+//         for(int j=5; j>0; j--){
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
 public class strings {
 public static void main(String[] args){
-    String name = "uttam ptara";
-    for(int i = 0; i<name.length(); i++){
-        System.out.println(name.charAt(i));
-    }
-    }
+	for(int i = 1; i<= 5; i++){
+		for(int j = 1; j<=i; j++){
+		  System.out.print(j+" ");
+		}
+		System.out.println();
+	  }
+	  for(int i = 1; i<= 5; i++){
+		for(int j = i; j>=1; j--){
+		  System.out.print(j+" ");
+		}
+		System.out.println();
+	  }
+	}
 }
