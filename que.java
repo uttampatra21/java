ab5163c14a0c1099f17ba0aecd0c02fbfb90211d
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-26 12:15:07 +0530

    0

--- que.java
+++ que.java
@@ -169,15 +169,70 @@ import javax.xml.transform.Source;
 //     }   
 // }
 
+
+// public class que {
+// public static void main(String[] args){
+//     int count = 0;
+//     for(int i=0; i<4; i++){
+//         for(int j=1; j<=4; j++){
+//             count++;
+//             System.out.print(count+" ");
+//         }
+//         System.out.println();
+//       }
+//     }
+// }
+
+// public class que {
+// public static void main(String[] args){
+//     int count = 0;
+//     for(int i= 1; i<=6; i++){
+//         for(int j = 1; j<=i; j++){
+
+//         }
+//     }
+//     }
+// }
+
+// public class que {
+// public static void main(String[] args){
+//     for(int i = 1; i<=4; i++){
+//         for(int j = 1; j<=5; j++){
+//             if(i==1|| j==1|| j==5 || i==5){
+//                 System.out.print("*");
+//             }else{
+//                 System.out.print(" ");
+//             }
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
+// public class que {
+// public static void main(String[] args){
+//     for(int i=1; i<=4; i++){
+//         for(int j=1; j<=i; j++){
+//             System.out.print(j+" ");
+//         }
+//         System.out.println();
+//     }
+//     }
+// }
+
 public class que {
 public static void main(String[] args){
-    int count = 0;
-    for(int i=0; i<4; i++){
-        for(int j=1; j<=4; j++){
-            count++;
-            System.out.print(count+" ");
+    for(int i=5; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print(j);
         }
         System.out.println();
     }
+    for(int i=1; i<=4; i++){
+                for(int j=1; j<=i; j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
     }
 }
