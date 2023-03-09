4b52dac11db7a6b2c2532fd24166887fb4e74499
uttam kumar patra <dev.uttampatra@gmail.com>
2023-03-09 19:26:27 +0530

    0

--- que.java
+++ que.java
@@ -748,14 +748,13 @@ import javax.xml.transform.Source;
 //     }
 // }
 
-public class que {
-public static void main(String[] args){
-    for (int i = 1; i <=5; i++) {
-        for(int j= 1; j<=5; j++) {
-            System.out.print("* ");
-        }
-        System.out.println();
-    }
-        
-    }
-    }
+// public class que {
+// public static void main(String[] args){
+//     for (int i = 1; i <=5; i++) {
+//         for(int j= 1; j<=5; j++) {
+//             System.out.print("* ");
+//         }
+//         System.out.println();
+//     }
+//     }
+//     }
