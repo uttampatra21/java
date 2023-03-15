44fa2f74c82f8ee77caf36d7d3ee20a8b0581cc0
uttam kumar patra <dev.uttampatra@gmail.com>
2023-03-15 14:07:33 +0530

    0

--- two.java
+++ two.java
@@ -1,9 +1,25 @@
+// public class two {
+// public static void main(String[] args){
+//     if(4>5){
+//         System.out.println("true");
+//     }else{
+//         System.out.println("false");
+//     }
+//     }
+// }
+
 public class two {
 public static void main(String[] args){
-    if(4>5){
-        System.out.println("true");
-    }else{
-        System.out.println("false");
+    int []Arr = {1,3,5};
+    int []Arr2 = {1,33,35};
+    int count =0;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(Arr[i]==Arr2[j]){
+                count = Arr[i];
+            }
+        }
     }
+    System.out.println(count);
     }
 }
