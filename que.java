6424cf06c32b0fe45bf539aae77846cbd53eea56
uttam kumar patra <dev.uttampatra@gmail.com>
2023-03-22 21:35:00 +0530

    0

--- que.java
+++ que.java
@@ -917,16 +917,16 @@
 //     }
 // }
 
-public class que {
-public static void main(String[] args){
-    String name = "aman";
-    for(int i=0; i<name.length(); i++){
-        for(int j=i+1; j<=name.length(); j++){
+// public class que {
+// public static void main(String[] args){
+//     String name = "aman";
+//     for(int i=0; i<name.length(); i++){
+//         for(int j=i+1; j<=name.length(); j++){
            
-        }
-    }
-}
-}
+//         }
+//     }
+// }
+// }
 
 // public class que {
 // public static void main(String[] args){
@@ -971,3 +971,17 @@ public static void main(String[] args){
 // }
 
 // sout
+
+
+public class que {
+public static void main(String[] args){
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int max = arr[0];
+    for(int i=0; i<arr.length; i++){
+        if(arr[i] >max){
+            max = arr[i];
+        }
+    }
+    System.out.println(max);
+    }
+}
