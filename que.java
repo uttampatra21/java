83d11a56fb00fd33b67e73336f2bddab34a3e8b5
uttam kumar patra <dev.uttampatra@gmail.com>
2023-03-25 13:32:14 +0530

    0

--- que.java
+++ que.java
@@ -1127,12 +1127,13 @@
 
 public class que {
 public static void main(String[] args){
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                System.out.print(" ");
-            }
-            for(int j=0; j<i; j++){
-                System.out.print("* ");
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                if(i==1 || j==1 || i==4|| j==5){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
